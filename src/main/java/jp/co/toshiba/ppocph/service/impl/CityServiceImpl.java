14094d8ce8f3746e9e36a9015656de6f08c11b61
ArkamaHozota <arkamahozota@hotmail.com>
2024-05-16 10:30:36 +0900

    1.02

--- src/main/java/jp/co/toshiba/ppocph/service/impl/CityServiceImpl.java
+++ src/main/java/jp/co/toshiba/ppocph/service/impl/CityServiceImpl.java
@@ -9,13 +9,15 @@ import org.jooq.DSLContext;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
-import jp.co.toshiba.ppocph.dto.CitiesRecordDto;
+import jp.co.toshiba.ppocph.common.PgCrowdConstants;
 import jp.co.toshiba.ppocph.dto.CityDto;
+import jp.co.toshiba.ppocph.jooq.Keys;
 import jp.co.toshiba.ppocph.jooq.tables.records.CitiesRecord;
 import jp.co.toshiba.ppocph.service.ICityService;
 import jp.co.toshiba.ppocph.utils.CommonProjectUtils;
 import jp.co.toshiba.ppocph.utils.Pagination;
 import jp.co.toshiba.ppocph.utils.ResultDto;
+import jp.co.toshiba.ppocph.utils.SecondBeanUtils;
 import jp.co.toshiba.ppocph.utils.SnowflakeUtils;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -41,89 +43,78 @@ public final class CityServiceImpl implements ICityService {
 				.on(CITIES.DISTRICT_ID.eq(DISTRICTS.ID)).where(DISTRICTS.ID.eq(districtId)).and(CITIES.NAME.eq(name))
 				.fetchSingle().into(Integer.class);
 		if (cityNameCount > 0) {
-			return ResultDto.failed(OgumaProjectConstants.MESSAGE_CITY_NAME_DUPLICATED);
+			return ResultDto.failed(PgCrowdConstants.MESSAGE_CITY_NAME_DUPLICATED);
 		}
 		return ResultDto.successWithoutData();
 	}
 
 	@Override
 	public Pagination<CityDto> getCitiesByKeyword(final Integer pageNum, final String keyword) {
-		final int offset = (pageNum - 1) * OgumaProjectConstants.DEFAULT_PAGE_SIZE;
+		final int offset = (pageNum - 1) * PgCrowdConstants.DEFAULT_PAGE_SIZE;
 		if (CommonProjectUtils.isEmpty(keyword)) {
 			final Integer totalRecords = this.dslContext.selectCount().from(CITIES).innerJoin(DISTRICTS)
 					.on(DISTRICTS.ID.eq(CITIES.DISTRICT_ID))
-					.where(CITIES.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL)).fetchSingle()
+					.where(CITIES.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL)).fetchSingle()
 					.into(Integer.class);
-			final List<CitiesRecordDto> citiesRecords = this.dslContext
-					.select(CITIES, DISTRICTS.NAME.as("districtName")).from(CITIES).innerJoin(DISTRICTS)
-					.on(DISTRICTS.ID.eq(CITIES.DISTRICT_ID))
-					.where(CITIES.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
-					.limit(OgumaProjectConstants.DEFAULT_PAGE_SIZE).offset(offset).fetchInto(CitiesRecordDto.class);
-			final List<CityDto> cityDtos = citiesRecords.stream()
-					.map(item -> new CityDto(item.getId(), item.getName(), item.getDistrictId(),
-							item.getPronunciation(), item.getDistrictName(), item.getPopulation(), item.getCityFlag()))
-					.toList();
-			return Pagination.of(cityDtos, totalRecords, pageNum, OgumaProjectConstants.DEFAULT_PAGE_SIZE);
+			final List<CityDto> cityDtos = this.dslContext
+					.select(CITIES.ID, CITIES.NAME, CITIES.DISTRICT_ID, CITIES.PRONUNCIATION, CITIES.CITY_FLAG,
+							CITIES.POPULATION, DISTRICTS.NAME.as("districtName"))
+					.from(CITIES).innerJoin(DISTRICTS).onKey(Keys.CITIES__FK_CITIES_DISTRICTS)
+					.where(CITIES.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL))
+					.limit(PgCrowdConstants.DEFAULT_PAGE_SIZE).offset(offset).fetchInto(CityDto.class);
+			return Pagination.of(cityDtos, totalRecords, pageNum, PgCrowdConstants.DEFAULT_PAGE_SIZE);
 		}
 		final String searchStr = CommonProjectUtils.getDetailKeyword(keyword);
 		final Integer totalRecords = this.dslContext.selectCount().from(CITIES).innerJoin(DISTRICTS)
 				.on(DISTRICTS.ID.eq(CITIES.DISTRICT_ID))
-				.where(CITIES.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL)).and(CITIES.NAME.like(searchStr)
+				.where(CITIES.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL)).and(CITIES.NAME.like(searchStr)
 						.or(CITIES.PRONUNCIATION.like(searchStr)).or(DISTRICTS.NAME.like(searchStr)))
 				.fetchSingle().into(Integer.class);
-		final List<CitiesRecordDto> citiesRecords = this.dslContext.select(CITIES, DISTRICTS.NAME.as("districtName"))
-				.from(CITIES).innerJoin(DISTRICTS).on(DISTRICTS.ID.eq(CITIES.DISTRICT_ID))
-				.where(CITIES.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
+		final List<CityDto> cityDtos = this.dslContext
+				.select(CITIES.ID, CITIES.NAME, CITIES.DISTRICT_ID, CITIES.PRONUNCIATION, CITIES.CITY_FLAG,
+						CITIES.POPULATION, DISTRICTS.NAME.as("districtName"))
+				.from(CITIES).innerJoin(DISTRICTS).onKey(Keys.CITIES__FK_CITIES_DISTRICTS)
+				.where(CITIES.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL))
 				.and(CITIES.NAME.like(searchStr).or(CITIES.PRONUNCIATION.like(searchStr))
 						.or(DISTRICTS.NAME.like(searchStr)))
-				.limit(OgumaProjectConstants.DEFAULT_PAGE_SIZE).offset(offset).fetchInto(CitiesRecordDto.class);
-		final List<CityDto> cityDtos = citiesRecords
-				.stream().map(item -> new CityDto(item.getId(), item.getName(), item.getDistrictId(),
-						item.getPronunciation(), item.getDistrictName(), item.getPopulation(), item.getCityFlag()))
-				.toList();
-		return Pagination.of(cityDtos, totalRecords, pageNum, OgumaProjectConstants.DEFAULT_PAGE_SIZE);
+				.limit(PgCrowdConstants.DEFAULT_PAGE_SIZE).offset(offset).fetchInto(CityDto.class);
+		return Pagination.of(cityDtos, totalRecords, pageNum, PgCrowdConstants.DEFAULT_PAGE_SIZE);
 	}
 
 	@Override
 	public ResultDto<String> remove(final Long id) {
-		this.dslContext.update(CITIES).set(CITIES.DELETE_FLG, OgumaProjectConstants.LOGIC_DELETE_FLG)
-				.where(CITIES.ID.eq(id)).execute();
+		this.dslContext.update(CITIES).set(CITIES.DELETE_FLG, PgCrowdConstants.LOGIC_DELETE_FLG).where(CITIES.ID.eq(id))
+				.execute();
 		return ResultDto.successWithoutData();
 	}
 
 	@Override
 	public void save(final CityDto cityDto) {
 		final CitiesRecord citiesRecord = this.dslContext.newRecord(CITIES);
+		SecondBeanUtils.copyNullableProperties(cityDto, citiesRecord);
 		citiesRecord.setId(SnowflakeUtils.snowflakeId());
-		citiesRecord.setName(cityDto.name());
-		citiesRecord.setDistrictId(cityDto.districtId());
-		citiesRecord.setPronunciation(cityDto.pronunciation());
-		citiesRecord.setPopulation(cityDto.population());
-		citiesRecord.setCityFlag(cityDto.cityFlag());
-		citiesRecord.setDeleteFlg(OgumaProjectConstants.LOGIC_DELETE_INITIAL);
+		citiesRecord.setDeleteFlg(PgCrowdConstants.LOGIC_DELETE_INITIAL);
 		citiesRecord.insert();
 	}
 
 	@Override
 	public ResultDto<String> update(final CityDto cityDto) {
 		final CitiesRecord citiesRecord = this.dslContext.selectFrom(CITIES)
-				.where(CITIES.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL)).and(CITIES.ID.eq(cityDto.id()))
-				.fetchSingle();
-		final CityDto aCityDto = new CityDto(citiesRecord.getId(), citiesRecord.getName(), citiesRecord.getDistrictId(),
-				citiesRecord.getPronunciation(), null, citiesRecord.getPopulation(), null);
+				.where(CITIES.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL))
+				.and(CITIES.ID.eq(Long.parseLong(cityDto.getId()))).fetchSingle();
+		final CityDto aCityDto = new CityDto();
+		SecondBeanUtils.copyNullableProperties(citiesRecord, aCityDto);
+		aCityDto.setId(citiesRecord.getId().toString());
 		if (CommonProjectUtils.isEqual(aCityDto, cityDto)) {
-			return ResultDto.failed(OgumaProjectConstants.MESSAGE_STRING_NOCHANGE);
+			return ResultDto.failed(PgCrowdConstants.MESSAGE_STRING_NOCHANGE);
 		}
-		citiesRecord.setName(cityDto.name());
-		citiesRecord.setDistrictId(cityDto.districtId());
-		citiesRecord.setPopulation(cityDto.population());
-		citiesRecord.setPronunciation(cityDto.pronunciation());
+		SecondBeanUtils.copyNullableProperties(cityDto, citiesRecord);
 		try {
 			this.dslContext.update(CITIES).set(citiesRecord)
-					.where(CITIES.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
+					.where(CITIES.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL))
 					.and(CITIES.ID.eq(citiesRecord.getId())).execute();
 		} catch (final DataIntegrityViolationException e) {
-			return ResultDto.failed(OgumaProjectConstants.MESSAGE_CITY_NAME_DUPLICATED);
+			return ResultDto.failed(PgCrowdConstants.MESSAGE_CITY_NAME_DUPLICATED);
 		}
 		return ResultDto.successWithoutData();
 	}
--- src/main/java/jp/co/toshiba/ppocph/service/impl/DistrictServiceImpl.java
+++ src/main/java/jp/co/toshiba/ppocph/service/impl/DistrictServiceImpl.java
@@ -3,23 +3,27 @@ package jp.co.toshiba.ppocph.service.impl;
 import static jp.co.toshiba.ppocph.jooq.Tables.CITIES;
 import static jp.co.toshiba.ppocph.jooq.Tables.DISTRICTS;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.jooq.DSLContext;
+import org.jooq.Record2;
+import org.jooq.Table;
 import org.jooq.impl.DSL;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
-import jp.co.toshiba.ppocph.common.OgumaProjectConstants;
+import jp.co.toshiba.ppocph.common.PgCrowdConstants;
 import jp.co.toshiba.ppocph.dto.DistrictDto;
-import jp.co.toshiba.ppocph.dto.DistrictsRecordDto;
+import jp.co.toshiba.ppocph.jooq.Keys;
 import jp.co.toshiba.ppocph.jooq.tables.records.DistrictsRecord;
 import jp.co.toshiba.ppocph.service.IDistrictService;
 import jp.co.toshiba.ppocph.utils.CommonProjectUtils;
 import jp.co.toshiba.ppocph.utils.Pagination;
 import jp.co.toshiba.ppocph.utils.ResultDto;
+import jp.co.toshiba.ppocph.utils.SecondBeanUtils;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
@@ -42,91 +46,87 @@ public final class DistrictServiceImpl implements IDistrictService {
 	public List<DistrictDto> getDistrictsByCityId(final String cityId) {
 		final List<DistrictDto> districtDtos = new ArrayList<>();
 		final List<DistrictsRecord> districtRecords = this.dslContext.selectFrom(DISTRICTS)
-				.where(DISTRICTS.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
+				.where(DISTRICTS.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL)).orderBy(DISTRICTS.ID.asc())
 				.fetchInto(DistrictsRecord.class);
-		final List<DistrictDto> districtDtos1 = districtRecords
-				.stream().map(item -> new DistrictDto(item.getId(), item.getName(), item.getShutoId(), null,
-						item.getChiho(), null, item.getDistrictFlag()))
-				.sorted(Comparator.comparingLong(DistrictDto::id)).toList();
+		final List<DistrictDto> districtDtos1 = districtRecords.stream().map(item -> {
+			final DistrictDto districtDto = new DistrictDto();
+			SecondBeanUtils.copyNullableProperties(item, districtDto);
+			districtDto.setId(item.getId().toString());
+			return districtDto;
+		}).collect(Collectors.toList());
+		final DistrictDto districtDto = new DistrictDto();
 		if (!CommonProjectUtils.isDigital(cityId)) {
-			final DistrictDto districtDto = new DistrictDto(0L, OgumaProjectConstants.DEFAULT_ROLE_NAME, 0L,
-					CommonProjectUtils.EMPTY_STRING, CommonProjectUtils.EMPTY_STRING, null,
-					CommonProjectUtils.EMPTY_STRING);
-			districtDtos.add(districtDto);
-			districtDtos.addAll(districtDtos1);
-			return districtDtos;
+			districtDto.setId(String.valueOf(0L));
+			districtDto.setName(CommonProjectUtils.EMPTY_STRING);
+			districtDto.setChiho(CommonProjectUtils.EMPTY_STRING);
+		} else {
+			final DistrictsRecord districtsRecord = this.dslContext.select(DISTRICTS.fields()).from(DISTRICTS)
+					.innerJoin(CITIES).onKey(Keys.CITIES__FK_CITIES_DISTRICTS)
+					.where(DISTRICTS.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL))
+					.and(CITIES.ID.eq(Long.parseLong(cityId))).fetchSingle().into(DistrictsRecord.class);
+			SecondBeanUtils.copyNullableProperties(districtsRecord, districtDto);
+			districtDto.setId(districtsRecord.getId().toString());
 		}
-		final DistrictsRecord districtsRecord = this.dslContext.select(DISTRICTS).from(DISTRICTS).innerJoin(CITIES)
-				.on(CITIES.DISTRICT_ID.eq(DISTRICTS.ID))
-				.where(DISTRICTS.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
-				.and(CITIES.ID.eq(Long.parseLong(cityId))).fetchSingle().into(DistrictsRecord.class);
-		districtDtos
-				.add(new DistrictDto(districtsRecord.getId(), districtsRecord.getName(), districtsRecord.getShutoId(),
-						null, districtsRecord.getChiho(), null, districtsRecord.getDistrictFlag()));
+		districtDtos.add(districtDto);
 		districtDtos.addAll(districtDtos1);
-		return districtDtos.stream().distinct().toList();
+		return districtDtos.stream().distinct().collect(Collectors.toList());
 	}
 
 	@Override
 	public Pagination<DistrictDto> getDistrictsByKeyword(final Integer pageNum, final String keyword) {
-		final int offset = (pageNum - 1) * OgumaProjectConstants.DEFAULT_PAGE_SIZE;
+		final Table<Record2<Long, BigDecimal>> subQueryTable = this.dslContext
+				.select(CITIES.DISTRICT_ID.as("districtId"), DSL.sum(CITIES.POPULATION).as("population")).from(CITIES)
+				.where(CITIES.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL)).groupBy(CITIES.DISTRICT_ID)
+				.asTable();
+		final int offset = (pageNum - 1) * PgCrowdConstants.DEFAULT_PAGE_SIZE;
 		if (CommonProjectUtils.isEmpty(keyword)) {
 			final Integer totalRecords = this.dslContext.selectCount().from(DISTRICTS).innerJoin(CITIES)
 					.on(CITIES.ID.eq(DISTRICTS.SHUTO_ID))
-					.where(DISTRICTS.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL)).fetchSingle()
+					.where(DISTRICTS.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL)).fetchSingle()
 					.into(Integer.class);
-			final List<DistrictsRecordDto> districtsRecords = this.dslContext
-					.select(DISTRICTS, CITIES.NAME.as("shutoName")).from(DISTRICTS).innerJoin(CITIES)
-					.on(CITIES.ID.eq(DISTRICTS.SHUTO_ID))
-					.where(DISTRICTS.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
-					.limit(OgumaProjectConstants.DEFAULT_PAGE_SIZE).offset(offset).fetchInto(DistrictsRecordDto.class);
-			final List<DistrictDto> districtDtos = districtsRecords.stream().map(item -> {
-				final Long population = this.dslContext.select(DSL.sum(CITIES.POPULATION)).from(CITIES)
-						.where(CITIES.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
-						.and(CITIES.DISTRICT_ID.eq(item.getId())).fetchSingle().into(Long.class);
-				return new DistrictDto(item.getId(), item.getName(), item.getShutoId(), item.getShutoName(),
-						item.getChiho(), population, item.getDistrictFlag());
-			}).toList();
-			return Pagination.of(districtDtos, totalRecords, pageNum, OgumaProjectConstants.DEFAULT_PAGE_SIZE);
+			final List<DistrictDto> districtDtos = this.dslContext
+					.select(DISTRICTS.ID, DISTRICTS.NAME, DISTRICTS.SHUTO_ID, CITIES.NAME.as("shutoName"),
+							DISTRICTS.CHIHO, subQueryTable.field("population"), DISTRICTS.DISTRICT_FLAG)
+					.from(DISTRICTS).innerJoin(CITIES).on(CITIES.ID.eq(DISTRICTS.SHUTO_ID)).innerJoin(subQueryTable)
+					.on(DISTRICTS.ID.eq(subQueryTable.field("districtId", Long.class)))
+					.where(DISTRICTS.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL))
+					.limit(PgCrowdConstants.DEFAULT_PAGE_SIZE).offset(offset).fetchInto(DistrictDto.class);
+			return Pagination.of(districtDtos, totalRecords, pageNum, PgCrowdConstants.DEFAULT_PAGE_SIZE);
 		}
 		final String searchStr = CommonProjectUtils.getDetailKeyword(keyword);
 		final Integer totalRecords = this.dslContext.selectCount().from(DISTRICTS).innerJoin(CITIES)
 				.on(CITIES.ID.eq(DISTRICTS.SHUTO_ID))
-				.where(DISTRICTS.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
+				.where(DISTRICTS.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL))
 				.and(DISTRICTS.NAME.like(searchStr).or(CITIES.NAME.like(searchStr))).fetchSingle().into(Integer.class);
-		final List<DistrictsRecordDto> districtsRecords = this.dslContext.select(DISTRICTS, CITIES.NAME.as("shutoName"))
-				.from(DISTRICTS).innerJoin(CITIES).on(CITIES.ID.eq(DISTRICTS.SHUTO_ID))
-				.where(DISTRICTS.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
+		final List<DistrictDto> districtDtos = this.dslContext
+				.select(DISTRICTS.ID, DISTRICTS.NAME, DISTRICTS.SHUTO_ID, CITIES.NAME.as("shutoName"), DISTRICTS.CHIHO,
+						subQueryTable.field("population"), DISTRICTS.DISTRICT_FLAG)
+				.from(DISTRICTS).innerJoin(CITIES).on(CITIES.ID.eq(DISTRICTS.SHUTO_ID)).innerJoin(subQueryTable)
+				.on(DISTRICTS.ID.eq(subQueryTable.field("districtId", Long.class)))
+				.where(DISTRICTS.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL))
 				.and(DISTRICTS.NAME.like(searchStr).or(CITIES.NAME.like(searchStr)))
-				.limit(OgumaProjectConstants.DEFAULT_PAGE_SIZE).offset(offset).fetchInto(DistrictsRecordDto.class);
-		final List<DistrictDto> districtDtos = districtsRecords.stream().map(item -> {
-			final Long population = this.dslContext.select(DSL.sum(CITIES.POPULATION)).from(CITIES)
-					.where(CITIES.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
-					.and(CITIES.DISTRICT_ID.eq(item.getId())).fetchSingle().into(Long.class);
-			return new DistrictDto(item.getId(), item.getName(), item.getShutoId(), item.getShutoName(),
-					item.getChiho(), population, item.getDistrictFlag());
-		}).toList();
-		return Pagination.of(districtDtos, totalRecords, pageNum, OgumaProjectConstants.DEFAULT_PAGE_SIZE);
+				.limit(PgCrowdConstants.DEFAULT_PAGE_SIZE).offset(offset).fetchInto(DistrictDto.class);
+		return Pagination.of(districtDtos, totalRecords, pageNum, PgCrowdConstants.DEFAULT_PAGE_SIZE);
 	}
 
 	@Override
 	public ResultDto<String> update(final DistrictDto districtDto) {
 		final DistrictsRecord districtsRecord = this.dslContext.selectFrom(DISTRICTS)
-				.where(DISTRICTS.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
-				.and(DISTRICTS.ID.eq(districtDto.id())).fetchSingle();
-		final DistrictDto aDistrictDto = new DistrictDto(districtsRecord.getId(), districtsRecord.getName(), null, null,
-				districtsRecord.getChiho(), null, null);
+				.where(DISTRICTS.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL))
+				.and(DISTRICTS.ID.eq(Long.parseLong(districtDto.getId()))).fetchSingle();
+		final DistrictDto aDistrictDto = new DistrictDto();
+		SecondBeanUtils.copyNullableProperties(districtsRecord, aDistrictDto);
+		aDistrictDto.setId(districtsRecord.getId().toString());
 		if (CommonProjectUtils.isEqual(aDistrictDto, districtDto)) {
-			return ResultDto.failed(OgumaProjectConstants.MESSAGE_STRING_NOCHANGE);
+			return ResultDto.failed(PgCrowdConstants.MESSAGE_STRING_NOCHANGE);
 		}
-		districtsRecord.setName(districtDto.name());
-		districtsRecord.setChiho(districtDto.chiho());
+		SecondBeanUtils.copyNullableProperties(districtDto, districtsRecord);
 		try {
 			this.dslContext.update(DISTRICTS).set(districtsRecord)
-					.where(DISTRICTS.DELETE_FLG.eq(OgumaProjectConstants.LOGIC_DELETE_INITIAL))
+					.where(DISTRICTS.DELETE_FLG.eq(PgCrowdConstants.LOGIC_DELETE_INITIAL))
 					.and(DISTRICTS.ID.eq(districtsRecord.getId())).execute();
 		} catch (final DataIntegrityViolationException e) {
-			return ResultDto.failed(OgumaProjectConstants.MESSAGE_DISTRICT_NAME_DUPLICATED);
+			return ResultDto.failed(PgCrowdConstants.MESSAGE_DISTRICT_NAME_DUPLICATED);
 		}
 		return ResultDto.successWithoutData();
 	}
