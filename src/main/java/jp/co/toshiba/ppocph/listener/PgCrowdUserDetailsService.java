52de630d3a9698840c4cf5a28aba74c561803567
ArkamaHozota <arkamahozota@hotmail.com>
2024-05-17 10:31:12 +0900

    4.04

--- src/main/java/jp/co/toshiba/ppocph/listener/PgCrowdUserDetailsService.java
+++ src/main/java/jp/co/toshiba/ppocph/listener/PgCrowdUserDetailsService.java
@@ -5,7 +5,6 @@ import static jp.co.toshiba.ppocph.jooq.Tables.EMPLOYEES;
 import static jp.co.toshiba.ppocph.jooq.Tables.EMPLOYEE_ROLE;
 import static jp.co.toshiba.ppocph.jooq.Tables.ROLE_AUTH;
 
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -67,12 +66,7 @@ public class PgCrowdUserDetailsService implements UserDetailsService {
 		}
 		final EmployeeDto employeeDto = new EmployeeDto();
 		employeeDto.setId(employeesRecord.getId().toString());
-		employeeDto.setLoginAccount(employeesRecord.getLoginAccount());
 		employeeDto.setUsername(employeesRecord.getUsername());
-		employeeDto.setPassword(employeesRecord.getPassword());
-		employeeDto.setEmail(employeesRecord.getEmail());
-		employeeDto.setDateOfBirth(DateTimeFormatter.ofPattern("yyyy-MM-dd").format(employeesRecord.getDateOfBirth()));
-		employeeDto.setRoleId(employeeRoleRecord.getRoleId().toString());
 		final List<SimpleGrantedAuthority> authorities = authoritiesRecords.stream()
 				.map(item -> new SimpleGrantedAuthority(item.getName())).collect(Collectors.toList());
 		return new SecurityAdmin(employeeDto, authorities);
